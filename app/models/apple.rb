595fe15f645f64c700ce8def8894be79c6e4e6f6
kazunori kim <kazukim.1029@gmail.com>
2017-06-22 06:27:20 +0000

    Add validation

--- app/models/apple.rb
+++ app/models/apple.rb
@@ -1,2 +1,5 @@
 class Apple < ApplicationRecord
+    validates :pine , length: { maximum: 32 } , presence: true
+ validates :grape , length: { maximum: 256 } , presence: true
+ validates :weight , length: { maximum: 32 }
 end
